92d8ad7045bf9930f053de94078232f21ed15e65
Julian Cox <julian.cox@webspeed.co.nz>
2024-05-08 11:12:20 +1200

    added extent to map connector

--- src/lib/arcgis-map.js
+++ src/lib/arcgis-map.js
@@ -637,6 +637,22 @@ export const arcgisMap = function (config = {}) {
             calibrationBase: getBase(calibration.description),
             capture: "capture",
           });
+
+           const buffer = {
+            xmin: -0.005,
+            ymin: -0.005,
+            xmax: 0.005,
+            ymax: 0.005,
+          };
+          const props = Object.keys(buffer);
+          const extent = {};
+          for (let i = 0; i < props.length; i++) {
+            extent[props[i]] =
+              parseFloat(layer.fullExtent[props[i]]) + buffer[props[i]];
+          }
+
+          mapView.extent = extent;
+
         });
 
         const pointFeaturesUrl = `${src}/1`;
