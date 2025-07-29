73758f52983f993723ae65668c6ce753fc5c5b28
Julian Cox <julian.cox@webspeed.co.nz>
2025-07-29 16:16:40 +1200

    checked for target for keys and dont hijack if input

--- src/lib/arcgis-map.js
+++ src/lib/arcgis-map.js
@@ -432,6 +432,7 @@ export const arcgisMap = function (config = {}) {
       });
 
       mapView.on("key-down", (event) => {
+            if (event.target.closest('input')) return;
         // stop map keyboard navigation when the viewer is visible so we can use it for the viewer
         // (trapping all even thought prev and next plugin may not be included)
         const prohibitedKeys = [
